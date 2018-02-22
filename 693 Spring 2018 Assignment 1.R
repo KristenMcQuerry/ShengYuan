c45c3d0545184d27d994107190cc349c33a760b5
SHENG YUAN <sheng.yuan@uky.edu>
2018-02-21 22:25:15 -0500

    version 1.0.0

--- 693 Spring 2018 Assignment 1.R
+++ 693 Spring 2018 Assignment 1.R
@@ -1,6 +1,6 @@
 ##install packages
-install.packages("tidyverse")
-install.packages("nycflights13")
+#install.packages("tidyverse")
+#install.packages("nycflights13")
 
 ## load libraries
 library(tidyverse)
@@ -8,3 +8,51 @@ library(nycflights13)
 
 ## dataset for assignment
 flights
+
+## arrival delay of two or more hours
+flights %>%
+    filter(arr_delay >= 120)
+
+## Flew to Houston
+flights %>%
+    filter(dest == 'HOU' | dest == 'IAH')
+
+## operated by United, American, or Delta
+flights %>%
+    filter(carrier == 'AA' | carrier == 'UA' | carrier == 'DL')
+
+## Departed in summer
+flights %>%
+    filter(month == 6 | month == 7 | month == 8)
+
+## Arrived more than two hours late, but didn't leave late
+flights %>%
+    filter(arr_delay >= 120 & dep_delay <= 0)
+
+##  Were delayed by at least an hour, but made up over 30 minutes in flight
+flights %>%
+    filter(dep_delay >= 60) %>%
+    filter(arr_delay <= dep_delay-30)
+
+## Departed between midnight and 6 a.m. (inclusive)
+flights %>%
+    filter(dep_time >= 0 & dep_time <= 630)
+
+## Sort to find the most delayed flights. Find the flights that left earliest.
+flights %>%
+    arrange(dep_delay)
+
+flights %>%
+    arrange(desc(dep_delay))
+
+## Sort to find the fastest flights
+flights %>%
+    mutate(speed = distance / air_time) %>%
+    arrange(speed)
+
+## Which flights traveled the longest? Which traveled the shortest?
+flights %>%
+    arrange(distance)
+
+flights %>%
+    arrange(desc(distance))
